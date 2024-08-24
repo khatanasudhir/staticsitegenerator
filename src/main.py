17e1f0e861c701524310be52f25ddbf09966b15a
khatanasudhir <khatanasudhir786@gmail.com>
2024-08-24 09:39:59 +0530

    making static site generator

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+    text_node = TextNode('Example txt', 'bold', "http://bootdev.com")
+    
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
